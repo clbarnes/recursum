9794937e3c17d9bdfa9e18ba47ed133cf0f6ffaf
Chris Barnes <barnesc@janelia.hhmi.org>
2020-07-31 18:46:22 +0100

    Accept single-file and piped inputs

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # recursum
 
-Rust script to hash every file in a directory, quickly.
+Rust script to hash many files, quickly.
 
-Parallelises file discovery and hashing.
+There are 3 modes of operation.
+
+1. The original, and the root of the crate's name, is recursively checking every (non-symlink) file in a given directory tree.
+2. Hash a single given file.
+3. Take a list of files from stdin and hash each of them.
+
+Parallelises file discovery (in usage #1) and hashing (#1 and #3).
 [Default hasher](https://mollyrocket.com/meowhash) is not cryptographically secure.
 
 `"{path}"\t{hex_digest}` printed to stdout; progress information on stderr.
 
 Contributions welcome.
 
+## Usage
+
 ```
-recursum 0.1.0
-Recursively hash all files in a directory, fast.
+recursum
+Hash lots of files fast, in parallel.
 
 USAGE:
     recursum [OPTIONS] <input>
@@ -21,12 +29,25 @@ FLAGS:
     -V, --version    Prints version information
 
 OPTIONS:
-    -b, --buffer <buffer>           Buffer factor (how large inter-thread buffers should be, as a proportion of threads)
-                                    [default: 3]
     -d, --digest <digest-length>    Maximum length of output hash digests
     -t, --threads <threads>         Hashing threads
     -w, --walkers <walkers>         Directory-walking threads
 
 ARGS:
-    <input>    Input file
+    <input>    File name, directory name (every file recursively will be hashed, in depth first order), or - for
+               stdin
 ```
+
+Example:
+
+```sh
+fd --type file | recursum --threads 10 --digest 64 - > my_checksums.txt
+```
+
+This should be much more efficient, and with better logging, than using `--exec` or `| xargs`.
+
+## Notes
+
+In mode #1, there is backpressure which minimises wasted RAM (files are only discovered shortly before they're needed).
+In mode #3, this is not the case because by default, rust cannot use a full pipe buffer as backpressure.
+If the directory tree is very (*very*) large, and the files are listed faster than they're hashed (likely), there may be quite some RAM wastage.
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,52 @@
+use std;
 use std::collections::VecDeque;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::Read;
 use std::path::{Path, PathBuf};
+use std::ffi::OsString;
 
 use digest::{Digest, Output};
 use indicatif::{HumanBytes, HumanDuration, ProgressBar, ProgressStyle};
 use jwalk::{Parallelism, WalkDir};
 use meowhash::MeowHasher;
 use num_cpus;
-use pathdiff::diff_paths;
 use std::time::Instant;
 use structopt::StructOpt;
+use tokio;
+use tokio::io::AsyncBufReadExt;
 use tokio::runtime;
+use tokio::stream::{Stream, StreamExt};
 use tokio::sync::mpsc;
 
 const READ_BUFFER_SIZE: usize = 8 * 1024; // BufReader default, may want to increase
 const HASH_BUFFER_SIZE: usize = 1024;
 
+const BUFFER_PPN: f64 = 3.0;
+
+fn queue_length(n_jobs: usize) -> usize {
+    (n_jobs as f64 * BUFFER_PPN).ceil() as usize
+}
+
+fn stdin_paths() -> mpsc::UnboundedReceiver<PathBuf> {
+    let (sender, receiver) = mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        let stdin = tokio::io::BufReader::new(tokio::io::stdin());
+        let mut line_stream = stdin.lines();
+
+        while let Some(path_result) = line_stream.next().await {
+            sender.send(PathBuf::from(&path_result.unwrap())).unwrap();
+        }
+    });
+    receiver
+}
+
 // adapted from https://stackoverflow.com/a/58825638/2700168
-fn path_queue(
+fn walk_paths(
     root: PathBuf,
     queue_len: usize,
     parallelism: Parallelism,
 ) -> mpsc::Receiver<PathBuf> {
     let (mut sender, receiver) = mpsc::channel(queue_len);
-
     tokio::spawn(async move {
         for entry in WalkDir::new(root)
             .parallelism(parallelism)
@@ -42,70 +63,114 @@ fn path_queue(
     receiver
 }
 
-async fn hash_all(root: PathBuf, n_walkers: usize, queue_len: usize, truncate_to: Option<usize>) {
-    let mut receiver = path_queue(
-        root.clone(),
-        queue_len,
-        Parallelism::RayonNewPool(n_walkers),
-    );
-    let mut fut_queue = VecDeque::with_capacity(queue_len);
+struct ResultOutput {
+    started: Instant,
+    total_files: u64,
+    total_bytes: u64,
+    progress: Option<ProgressBar>,
+}
+
+impl ResultOutput {
+    // fn default() -> Self {
+    //     Self {
+    //         started: Instant::now(),
+    //         total_files: 0,
+    //         total_bytes: 0,
+    //         progress: None,
+    //     }
+    // }
+
+    // fn with_progress(pbar: ProgressBar) -> Self {
+    //     Self {
+    //         started: Instant::now(),
+    //         total_files: 0,
+    //         total_bytes: 0,
+    //         progress: Some(pbar),
+    //     }
+    // }
+
+    fn with_default_progress() -> Self {
+        let spinner_style =
+            ProgressStyle::default_spinner().template("{bytes} | {elapsed} | {bytes_per_sec} | {msg}");
+        let spinner = ProgressBar::new_spinner().with_style(spinner_style);
+        Self {
+            started: Instant::now(),
+            total_files: 0,
+            total_bytes: 0,
+            progress: Some(spinner),
+        }
+    }
+
+    fn handle_output(&mut self, path: &Path, hash: &str, size: u64) {
+        println!("{:?}\t{}", path, hash);
+        if let Some(ref mut p) = self.progress {
+            p.set_message(&format!("{} {:?}", HumanBytes(size), path));
+            p.inc(size);
+        }
+        self.total_files += 1;
+        self.total_bytes += size;
+    }
+
+    fn finish(&mut self) {
+        if let Some(ref mut p) = self.progress {
+            p.finish_and_clear();
+        }
+        let elapsed = Instant::now().duration_since(self.started);
+        let rate = (self.total_bytes as f64 / elapsed.as_secs_f64()).floor() as u64;
+        eprintln!(
+            "{} files ({}) hashed in {} ({}/s)",
+            self.total_files,
+            HumanBytes(self.total_bytes),
+            HumanDuration(elapsed),
+            HumanBytes(rate),
+        );
+    }
+}
+
+async fn hash_from_stream<S: Stream<Item = PathBuf> + Unpin>(
+    mut path_stream: S,
+    truncate_to: Option<usize>,
+    n_jobs: usize,
+) {
+    let mut output = ResultOutput::with_default_progress();
+
+    let mut fut_queue = VecDeque::with_capacity(n_jobs);
     let mut is_finished = false;
 
-    let spinner_style =
-        ProgressStyle::default_spinner().template("{bytes} | {elapsed} | {bytes_per_sec} | {msg}");
-    let spinner = ProgressBar::new_spinner().with_style(spinner_style);
-    let mut total_files: usize = 0;
-    let mut total_bytes: usize = 0;
-    let started = Instant::now();
+    let queue_len = queue_length(n_jobs);
 
+    // make sure there are n_jobs running before looking at results
     for _ in 0..queue_len {
-        if let Some(path) = receiver.recv().await {
+        if let Some(path) = path_stream.next().await {
+            // todo: factor out
             fut_queue.push_back(tokio::spawn(async move {
                 let (hash, size) = hash_file(path.as_path(), MeowHasher::new(), truncate_to);
                 (path, hash, size)
             }));
         } else {
-            is_finished = false;
+            // there were fewer than n_jobs to begin with
+            is_finished = true;
             break;
         }
     }
 
-    while !is_finished {
-        if let Some(path) = receiver.recv().await {
-            let output = fut_queue.pop_front().unwrap().await.unwrap();
-            spinner.set_message(&format!("{} {:?}", HumanBytes(output.2 as u64), output.0));
-            spinner.inc(output.2 as u64);
-            total_bytes += output.2;
-            total_files += 1;
-            println!("{:?}\t{}", diff_paths(output.0, &root).unwrap(), output.1);
+    if !is_finished {
+        // pop the first job off the queue when completed, spawn another and append to queue
+        while let Some(path) = path_stream.next().await {
+            let result = fut_queue.pop_front().unwrap().await.unwrap();
+            output.handle_output(result.0.as_path(), result.1.as_str(), result.2 as u64);
             fut_queue.push_back(tokio::spawn(async move {
                 let (hash, size) = hash_file(path.as_path(), MeowHasher::new(), truncate_to);
                 (path, hash, size)
             }));
-        } else {
-            is_finished = true;
         }
     }
 
     for fut in fut_queue.into_iter() {
-        let output = fut.await.unwrap();
-        spinner.set_message(&format!("{} {:?}", HumanBytes(output.2 as u64), output.0));
-        spinner.inc(output.2 as u64);
-        total_bytes += output.2;
-        total_files += 1;
-        println!("{:?}\t{}", diff_paths(output.0, &root).unwrap(), output.1);
+        let result = fut.await.unwrap();
+        output.handle_output(result.0.as_path(), result.1.as_str(), result.2 as u64);
     }
-    spinner.finish_and_clear();
-    let elapsed = Instant::now().duration_since(started);
-    let rate = (total_bytes as f64 / elapsed.as_secs_f64()).floor() as u64;
-    eprintln!(
-        "{} files ({}) hashed in {} ({}/s) from {:?}",
-        total_files,
-        HumanBytes(total_bytes as u64),
-        HumanDuration(elapsed),
-        HumanBytes(rate),
-        root
-    );
+    output.finish();
 }
 
 fn hash_file<D: Digest>(fpath: &Path, hasher: D, truncate: Option<usize>) -> (String, usize) {
@@ -120,7 +185,7 @@ fn hash_file<D: Digest>(fpath: &Path, hasher: D, truncate: Option<usize>) -> (St
 
 // adapted from https://rust-lang-nursery.github.io/rust-cookbook/cryptography/hashing.html#calculate-the-sha-256-digest-of-a-file
 fn hash_reader<R: Read, D: Digest>(reader: R, mut hasher: D) -> (Output<D>, usize) {
-    let mut buf_reader = BufReader::with_capacity(READ_BUFFER_SIZE, reader);
+    let mut buf_reader = std::io::BufReader::with_capacity(READ_BUFFER_SIZE, reader);
     let mut size = 0;
 
     let mut buf = [0; HASH_BUFFER_SIZE];
@@ -140,36 +205,86 @@ fn or_num_cpus(opt: Option<usize>) -> usize {
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "recursum", about = "Recursively hash all files in a directory, fast.")]
+#[structopt(
+    name = "recursum",
+    about = "Hash lots of files fast, in parallel."
+)]
 struct Opt {
-    /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
-    /// Directory-walking threads
+    /// File name, directory name (every file recursively will be hashed, in depth first order), or - for getting FILE list from stdin
+    #[structopt()]
+    input: OsString,
+    /// Directory-walking threads, if input is a directory
     #[structopt(short = "w", long = "walkers")]
     walkers: Option<usize>,
     /// Hashing threads
     #[structopt(short = "t", long = "threads")]
     threads: Option<usize>,
-    /// Buffer factor (how large inter-thread buffers should be, as a proportion of threads)
-    #[structopt(short = "b", long = "buffer", default_value = "3")]
-    buffer: f64,
     /// Maximum length of output hash digests
     #[structopt(short = "d", long = "digest")]
     digest_length: Option<usize>,
 }
 
+enum InputConfig {
+    Directory((usize, PathBuf, usize)),
+    Stdin(usize),
+}
+
+impl InputConfig {
+    async fn hash(&self, truncate_to: Option<usize>) {
+        match self {
+            Self::Directory((n_jobs, root, walkers)) => {
+                let stream = walk_paths(root.clone(), queue_length(*n_jobs), Parallelism::RayonNewPool(*walkers));
+                hash_from_stream(stream, truncate_to, *n_jobs).await;
+            }
+            Self::Stdin(n_jobs) => {
+                let stream = stdin_paths();
+                hash_from_stream(stream, truncate_to, *n_jobs).await;
+            },
+        }
+    }
+}
+
+fn handle_single_file(path: &Path, truncate: Option<usize>) {
+    let started = Instant::now();
+    let (digest, size) = hash_file(path, MeowHasher::new(), truncate);
+    println!("{:?}\t{}", path, digest);
+    let elapsed = Instant::now().duration_since(started);
+    let rate = (size as f64 / elapsed.as_secs_f64()).floor() as u64;
+    eprintln!(
+        "{} files ({}) hashed in {} ({}/s)",
+        1,
+        HumanBytes(size as u64),
+        HumanDuration(elapsed),
+        HumanBytes(rate),
+    );
+}
+
 fn main() {
     let opt = Opt::from_args();
     let threads = or_num_cpus(opt.threads);
-    let walkers: usize = or_num_cpus(opt.walkers);
 
-    let queue_len = (opt.buffer * threads as f64).ceil() as usize;
+    let input;
+    if opt.input == "-" {
+        input = InputConfig::Stdin(threads);
+    } else {
+        let path = PathBuf::from(opt.input);
+        if path.is_dir() {
+            let walkers = or_num_cpus(opt.walkers);
+            input = InputConfig::Directory((threads, path, walkers))
+        } else if path.is_file() {
+            handle_single_file(&path, opt.digest_length);
+            return
+        } else {
+            panic!("Given input is not a directory, file, or - for stdin");
+        }
+    }
+
     let mut rt = runtime::Builder::new()
         .enable_all()
         .threaded_scheduler()
         .core_threads(threads)
         .build()
         .unwrap();
-    rt.block_on(hash_all(opt.input, walkers, queue_len, opt.digest_length));
+
+    rt.block_on(input.hash(opt.digest_length));
 }
